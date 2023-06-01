08ec1f739b162dbd51bdb36905013ca3464dd4c8
Leon Schumacher <leonsch@protonmail.com>
2023-06-01 08:13:42 +0200

    templates: add default

---
+++ templates/default/flake.nix
@@ -0,0 +1,14 @@
+{
+  description = "A tiny flake";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        defaultPackage = example;
+      });
+}
