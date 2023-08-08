93f1becb2c6cdd0cbf16de504a5691330cc9f6a9
Leon Schumacher <leonsch@protonmail.com>
2023-08-08 07:56:41 +0200

    Add SwayAudioIdleInhibit

--- flake.nix
+++ flake.nix
@@ -3,28 +3,33 @@
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { nixpkgs, ... }: {
-    packages = let
-      dir = ./packages;
-      # TODO
-      result = {};
-    in result;
+  outputs = { self, nixpkgs, ... }: {
+    packages = self.inputs.flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        SwayAudioIdleInhibit = pkgs.callPackage ./packages/SwayAudioIdleInhibit.nix { };
+      }
+    );
 
-    templates = let
-      dir    = ./templates;
+    templates =
+      let
+        dir = ./templates;
 
-      mkPair = name: {
-        inherit name;
-        value = {
-          description = "A flake template for ${name}";
-          path = ./${dir}/${name};
+        mkPair = name: {
+          inherit name;
+          value = {
+            description = "A flake template for ${name}";
+            path = ./${dir}/${name};
+          };
         };
-      };
 
-      subdirs = builtins.readDir dir;
-      names   = builtins.attrNames subdirs;
-      pairs   = builtins.map mkPair names;
-      result  = builtins.listToAttrs pairs;
-    in result;
+        subdirs = builtins.readDir dir;
+        names = builtins.attrNames subdirs;
+        pairs = builtins.map mkPair names;
+        result = builtins.listToAttrs pairs;
+      in
+      result;
   };
 }
---
+++ packages/SwayAudioIdleInhibit.nix
@@ -0,0 +1,33 @@
+{ fetchFromGitHub
+, stdenv
+
+, meson
+, ninja
+, pkg-config
+
+, libpulseaudio
+, wayland
+, wayland-protocols
+}: stdenv.mkDerivation rec {
+  pname = "SwayAudioIdleInhibit";
+  version = "0.1.1";
+
+  src = fetchFromGitHub {
+    owner = "ErikReider";
+    repo = "SwayAudioIdleInhibit";
+    rev = "v${version}";
+    hash = "sha256-XUUUUeaXO7GApwe5vA/zxBrR1iCKvkQ/PMGelNXapbA=";
+  };
+
+  nativeBuildInputs = [
+    meson
+    ninja
+    pkg-config
+  ];
+
+  buildInputs = [
+    libpulseaudio
+    wayland
+    wayland-protocols
+  ];
+}
