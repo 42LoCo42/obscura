1eb08732e81254fc8e449ef1aee2549e47e7fd19
Leon Schumacher <leonsch@protonmail.com>
2023-08-21 07:25:07 +0200

    Update SwayAudioIdleInhibit

--- packages/SwayAudioIdleInhibit.nix
+++ packages/SwayAudioIdleInhibit.nix
@@ -8,15 +8,15 @@
 , libpulseaudio
 , wayland
 , wayland-protocols
-}: stdenv.mkDerivation rec {
+}: stdenv.mkDerivation {
   pname = "SwayAudioIdleInhibit";
-  version = "0.1.1";
+  version = "master";
 
   src = fetchFromGitHub {
     owner = "ErikReider";
     repo = "SwayAudioIdleInhibit";
-    rev = "v${version}";
-    hash = "sha256-XUUUUeaXO7GApwe5vA/zxBrR1iCKvkQ/PMGelNXapbA=";
+    rev = "c850bc4812216d03e05083c69aa05326a7fab9c7";
+    hash = "sha256-MKzyF5xY0uJ/UWewr8VFrK0y7ekvcWpMv/u9CHG14gs=";
   };
 
   nativeBuildInputs = [
