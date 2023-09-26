9ba854343d12d412b532138a7c8b23745f7082c5
Eleonora <leonsch@protonmail.com>
2023-09-26 08:43:03 +0200

    Remove packages for other architectures

--- flake.nix
+++ flake.nix
@@ -3,40 +3,37 @@
 
   inputs.argon-kg.url = "github:42loco42/argon-kg";
   inputs.argon-kg.inputs.nixpkgs.follows = "nixpkgs";
-  inputs.argon-kg.inputs.flake-utils.follows = "flake-utils";
-
-  inputs.flake-utils.url = "github:numtide/flake-utils";
 
   inputs.nimble.inputs.nixpkgs.follows = "nixpkgs";
 
-  outputs = { self, nixpkgs, nimble, flake-utils, ... }:
-    (flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        nimblePkgs = nimble.packages.${system};
-      in
-      {
-        packages = rec {
-          "9mount" = pkgs.callPackage ./packages/9mount { };
-          SwayAudioIdleInhibit = pkgs.callPackage ./packages/SwayAudioIdleInhibit.nix { };
-          XWaylandVideoBridge = pkgs.callPackage ./packages/XWaylandVideoBridge.nix { };
-          argon-kg = self.inputs.argon-kg.outputs.defaultPackage.${system};
-          boomer = pkgs.callPackage ./packages/boomer.nix { inherit nimblePkgs; };
-          certbot-dns-duckdns = pkgs.callPackage ./packages/certbot-dns-duckdns.nix { };
-          flameshot-fixed = pkgs.callPackage ./packages/flameshot-fixed.nix { };
-          fusepod = pkgs.callPackage ./packages/fusepod.nix { };
-          gtk4-layer-shell = pkgs.callPackage ./packages/gtk4-layer-shell.nix { };
-          m9u = pkgs.callPackage ./packages/m9u.nix { };
-          msp-cgt = pkgs.callPackage ./packages/msp-cgt.nix { };
-          mspgcc-ti = pkgs.callPackage ./packages/mspgcc-ti.nix { };
-          musializer = pkgs.callPackage ./packages/musializer.nix { };
-          samloader = pkgs.callPackage ./packages/samloader.nix { };
-          vulnix = pkgs.callPackage ./packages/vulnix.nix { };
-          wayland-shell = pkgs.callPackage ./packages/wayland-shell.nix {
-            inherit gtk4-layer-shell;
-          };
+  outputs = { self, nixpkgs, nimble, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+      nimblePkgs = nimble.packages.${system};
+    in
+    {
+      packages.${system} = rec {
+        "9mount" = pkgs.callPackage ./packages/9mount { };
+        SwayAudioIdleInhibit = pkgs.callPackage ./packages/SwayAudioIdleInhibit.nix { };
+        XWaylandVideoBridge = pkgs.callPackage ./packages/XWaylandVideoBridge.nix { };
+        argon-kg = self.inputs.argon-kg.outputs.defaultPackage.${system};
+        boomer = pkgs.callPackage ./packages/boomer.nix { inherit nimblePkgs; };
+        certbot-dns-duckdns = pkgs.callPackage ./packages/certbot-dns-duckdns.nix { };
+        flameshot-fixed = pkgs.callPackage ./packages/flameshot-fixed.nix { };
+        fusepod = pkgs.callPackage ./packages/fusepod.nix { };
+        gtk4-layer-shell = pkgs.callPackage ./packages/gtk4-layer-shell.nix { };
+        m9u = pkgs.callPackage ./packages/m9u.nix { };
+        msp-cgt = pkgs.callPackage ./packages/msp-cgt.nix { };
+        mspgcc-ti = pkgs.callPackage ./packages/mspgcc-ti.nix { };
+        musializer = pkgs.callPackage ./packages/musializer.nix { };
+        samloader = pkgs.callPackage ./packages/samloader.nix { };
+        vulnix = pkgs.callPackage ./packages/vulnix.nix { };
+        wayland-shell = pkgs.callPackage ./packages/wayland-shell.nix {
+          inherit gtk4-layer-shell;
         };
-      })) // {
+      };
+
       nixosModules = {
         "9mount" = import ./packages/9mount/module.nix {
           packages = self.outputs.packages;
