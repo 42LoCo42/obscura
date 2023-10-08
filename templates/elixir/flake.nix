96e1ee813a80eece77bce5d665822b7c0bceae7a
Leon Schumacher <leonsch@protonmail.com>
2023-10-08 10:08:29 +0200

    Add Elixir template

---
+++ templates/elixir/flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Example Elixir flake";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        defaultPackage = pkgs.beamPackages.mixRelease {
+          pname = "example";
+          version = "0.1.0";
+          src = ./.;
+        };
+
+        devShell = pkgs.mkShell {
+          packages = with pkgs; [
+            bashInteractive
+            beamPackages.hex
+            elixir-ls
+            elixir_1_15
+          ];
+        };
+      });
+}
