82af218598b861f6adae526bf60ed11ee84b561f
Leon Schumacher <leonsch@protonmail.com>
2024-10-05 17:01:22 +0200

    pug: fix IFD by switching to buildNpmPackage

--- packages/pug/default.nix
+++ packages/pug/default.nix
@@ -1,15 +1,22 @@
-pkgs: pkgs.mkYarnPackage rec {
-  pname = "pug3-cli";
-  version = "0e28930";
-  name = "${pname}-${version}";
+pkgs: pkgs.buildNpmPackage rec {
+  pname = "pug";
+  version = "3.0.1";
 
   src = pkgs.fetchFromGitHub {
     owner = "tokilabs";
-    repo = pname;
+    repo = "pug3-cli";
     rev = version;
     hash = "sha256-7qvHsP4fyrkOSTzl9zx1s/idFGBew8E0jpsedLiatOs=";
   };
-  yarnLock = ./yarn.lock;
+  patches = [ ./package-lock.patch ];
+
+  npmDepsHash = "sha256-6wbu2Rgy7aP1L99YE8KXjfwUaJmu869TEWQJdiHBzX8=";
+  dontNpmBuild = true;
+
+  postInstall = ''
+    gzip -9 man/pug3.0.2
+    install -Dm444 man/pug3.0.2.gz $out/share/man/man1/pug3.1.gz
+  '';
 
   meta = {
     mainProgram = "pug3";
