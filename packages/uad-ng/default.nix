bf999417667aa6c86894952a83f2180167480fbb
Leon Schumacher <leonsch@protonmail.com>
2024-10-10 22:59:19 +0200

    uad-ng: remove nightly clippy macros

--- packages/uad-ng/default.nix
+++ packages/uad-ng/default.nix
@@ -6,6 +6,7 @@ pkgs: pkgs.universal-android-debloater.overrideAttrs (old: rec {
     rev = "v${version}";
     hash = "sha256-o54gwFl2x0/nE1hiE5F8D18vQSNCKU9Oxiq8RA+yOoE=";
   };
+  patches = [ ./remove-nightly.patch ];
 
   cargoDeps = pkgs.rustPlatform.fetchCargoTarball {
     inherit src;
