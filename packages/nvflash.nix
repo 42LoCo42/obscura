7c7c1ceaa42307c2e68e200b418062f992a77272
Leon Schumacher <leonsch@protonmail.com>
2024-11-29 13:07:56 +0100

    nvflash: fix download by impersonating winget

--- packages/nvflash.nix
+++ packages/nvflash.nix
@@ -2,22 +2,21 @@ pkgs: pkgs.stdenv.mkDerivation rec {
   pname = "nvflash";
   version = "5.833.0";
 
-  src = pkgs.fetchzip {
-    url = "https://de1-dl.techpowerup.com/files/BMpwxX7RZOjgxUpWUaF17Q/1732858564/nvflash_5.833_linux.zip";
-    hash = "sha256-D9u+ahAz4ye7ybdtciI47uqoNqKGEq9tvPw5cC1c7i0=";
-  };
-
-  nativeBuildInputs = with pkgs; [
-    autoPatchelfHook
-  ];
-
-  buildInputs = with pkgs; [
-    libgcc
-  ];
-
-  installPhase = ''
-    install -Dm755 {,$out/bin/}nvflash
+  src = (pkgs.runCommand "nvflash" {
+    nativeBuildInputs = with pkgs; [ curl ];
+    outputHashAlgo = "sha256";
+    outputHash = "sha256-0fgLsOXFOrszkbV52zOGHkMxkg4NfYOfWbpk8DHBIcM=";
+  }) ''
+    export SSL_CERT_FILE="${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
+    curl https://de1-dl.techpowerup.com/files/nvflash_5.833_linux.zip \
+      -H "user-agent: winget" | zcat > $out
   '';
+  dontUnpack = true;
+
+  nativeBuildInputs = with pkgs; [ autoPatchelfHook ];
+  buildInputs = with pkgs; [ libgcc ];
+
+  installPhase = "install -Dm755 $src $out/bin/nvflash";
 
   meta = {
     description = "NVIDIA NVFlash is used to flash the graphics card BIOS on Ampere, Turing, Pascal and all older NVIDIA cards";
