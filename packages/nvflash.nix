8706a4e8269a8b9fc7fef51f34827a205ec0a4a6
Leon Schumacher <leonsch@protonmail.com>
2024-11-29 14:05:20 +0100

    nvflash: simplify fetcher

--- packages/nvflash.nix
+++ packages/nvflash.nix
@@ -2,21 +2,16 @@ pkgs: pkgs.stdenv.mkDerivation rec {
   pname = "nvflash";
   version = "5.833.0";
 
-  src = (pkgs.runCommand "nvflash" {
-    nativeBuildInputs = with pkgs; [ curl ];
-    outputHashAlgo = "sha256";
-    outputHash = "sha256-0fgLsOXFOrszkbV52zOGHkMxkg4NfYOfWbpk8DHBIcM=";
-  }) ''
-    export SSL_CERT_FILE="${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
-    curl https://de1-dl.techpowerup.com/files/nvflash_5.833_linux.zip \
-      -H "user-agent: winget" | zcat > $out
-  '';
-  dontUnpack = true;
+  src = pkgs.fetchzip {
+    url = "https://de1-dl.techpowerup.com/files/nvflash_5.833_linux.zip";
+    curlOptsList = [ "-H" "user-agent: winget" ];
+    hash = "sha256-D9u+ahAz4ye7ybdtciI47uqoNqKGEq9tvPw5cC1c7i0=";
+  };
 
   nativeBuildInputs = with pkgs; [ autoPatchelfHook ];
   buildInputs = with pkgs; [ libgcc ];
 
-  installPhase = "install -Dm755 $src $out/bin/nvflash";
+  installPhase = "install -Dm755 {,$out/bin/}nvflash";
 
   meta = {
     description = "NVIDIA NVFlash is used to flash the graphics card BIOS on Ampere, Turing, Pascal and all older NVIDIA cards";
