f1ff5cc286ce3f281223c5278959c334edd8e0bf
Leon Schumacher <leonsch@protonmail.com>
2024-12-29 19:45:42 +0100

    fix argtable2: missing include ctype.h

--- packages/argtable2/default.nix
+++ packages/argtable2/default.nix
@@ -7,6 +7,11 @@ pkgs: pkgs.stdenv.mkDerivation {
     hash = "sha256-j3fop87VMBr24i9HMC/bw7H/QfK4PEPHeuXKBBdx3b8=";
   };
 
+  patches = [
+    # fixes some implicit function declarations
+    ./0001-arg_int.c-include-ctype.h.patch
+  ];
+
   meta = {
     description = "An ANSI C command line parser";
     homepage = "https://argtable.sourceforge.io";
