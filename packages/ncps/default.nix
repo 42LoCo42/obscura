59e3b3a3c77ccc87d3c79f2d19cef9e78ff8ba05
Leon Schumacher <leonsch@protonmail.com>
2025-01-01 20:08:07 +0100

    ncps: 0.0.17 -> 0.0.19-unstable-2025-01-01

--- packages/ncps/default.nix
+++ packages/ncps/default.nix
@@ -1,12 +1,13 @@
 pkgs: pkgs.buildGoModule rec {
   pname = "ncps";
-  version = "0.0.17";
+  version = "0.0.19-unstable-2025-01-01";
 
   src = (pkgs.fetchFromGitHub {
     owner = "kalbasit";
     repo = pname;
-    rev = "v${version}";
-    hash = "sha256-OfbTmRpNJURoe2J14YI7+d1QGmGLjwc5Pamcph4Oaac=";
+    # rev = "v${version}";
+    rev = "53c506ce62d3a1c04b440a0ba53c5704fc14ae85";
+    hash = "sha256-UvOadj7c7+UBZxS9418brEAhxb5lok3B/fFOx8hFwWI=";
   }).overrideAttrs (old: {
     postFetch = old.postFetch + ''
       cd $out
@@ -16,7 +17,7 @@ pkgs: pkgs.buildGoModule rec {
   });
 
   ldflags = [ "-s" "-w" ];
-  vendorHash = "sha256-IFI9FDuRKn77dScuW0cl8hB2Va8Agw/wyhhEPDStQRI=";
+  vendorHash = "sha256-Lp7CY2/una+P+kDTQohffKFjOx48F6bwYebm7YmMopA=";
 
   postInstall =
     let inherit (pkgs.lib) getExe pipe; in
