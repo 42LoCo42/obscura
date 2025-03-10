b150e722eec8e38cd86c00e3b98c2890b3e47483
Leon Schumacher <leonsch@protonmail.com>
2025-03-10 12:09:32 +0100

    sillysecrets: 2.1.1 -> 2.1.2

--- packages/sillysecrets.nix
+++ packages/sillysecrets.nix
@@ -1,5 +1,5 @@
 pkgs:
-let src = builtins.getFlake "github:42loco42/sillysecrets/5363f2a51d5fdfc6e948b0df704349c20d9968a8"; in
+let src = builtins.getFlake "github:42loco42/sillysecrets/8038bd7d6610bb4599b3183fb0f224a80eac8e3b"; in
 src.packages.${pkgs.system}.default.overrideAttrs (old: {
   meta = old.meta // {
     description = "The silliest secret manager! :3";
