f330039089215bab2b6b7628bf5ad3a3edab98a1
Leon Schumacher <leonsch@protonmail.com>
2025-04-22 23:01:58 +0200

    add rustdesk-api

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ A personal collection of unusual things
   * substituter: `https://attic.eleonora.gay/default`
   * public key:  `default:3FYh8sZV8gWa7Jc5jlP7gZFK7pt3kaHRiV70ySaQ42g=`
 
-## 64 Packages
+## 65 Packages
 
 | Name | Version | Description | Homepage |
 |------|---------|-------------|----------|
@@ -60,6 +60,7 @@ A personal collection of unusual things
 |`redis-json`|`2.6.8`|RedisJSON - a JSON data type for Redis|https://github.com/RedisJSON/RedisJSON|
 |`rgssad`|`0.11`|Provide an interface to RGSS (ruby game scripting system) archive files|https://metacpan.org/dist/Archive-Rgssad|
 |`rom-parser`|`2017-03-31`|Parse & change GPU VBIOS files|https://github.com/awilliam/rom-parser|
+|`rustdesk-api`|`2.6.15`|Custom Rustdesk Api Server|https://github.com/lejianwen/rustdesk-api|
 |`sae_pk_gen`|`72e2975`|SAE-PK key generator|https://github.com/vanhoefm/hostap-wpa3|
 |`samloader`|`master`|Download Samsung firmware from official servers|https://github.com/samloader/samloader|
 |`sfnt2woff-zopfli`|`1.3.1`|WOFF utilities with Zopfli compression|https://github.com/bramstein/sfnt2woff-zopfli|
---
+++ packages/rustdesk-api/default.nix
@@ -0,0 +1,65 @@
+pkgs:
+let
+  owner = "lejianwen";
+
+  frontend = pkgs.buildNpmPackage rec {
+    pname = "rustdesk-api-web";
+    version = "1.2.0-unstable-2025-04-14";
+
+    src = pkgs.fetchFromGitHub {
+      inherit owner;
+      repo = pname;
+      rev = "83f25444ba952f21de2d73838d8982e768032f87";
+      hash = "sha256-boITBL8fnmx6RoKiOofOGfjrIJDRPjY4byjLDNg99Vg=";
+    };
+
+    npmDepsHash = "sha256-93qB65Ilghn5mCja3dU/Mib1iXFrYlfuYymoSaMsqYE=";
+
+    dontNpmInstall = true;
+    installPhase = "cp -r dist $out";
+
+    meta = {
+      description = "Frontend for rustdesk-api";
+      homepage = "https://github.com/lejianwen/rustdesk-api-web";
+    };
+  };
+
+  backend = pkgs.buildGoModule rec {
+    pname = "rustdesk-api";
+    version = "2.6.15";
+
+    src = pkgs.fetchFromGitHub {
+      inherit owner;
+      repo = pname;
+      rev = "v${version}";
+      hash = "sha256-13Hj1lf0PYQs+HowtfgFKf7f0sZwSZqJZbLVtzlV/XY=";
+    };
+
+    # the repo does not include these for some reason >_<
+    prePatch = "cp ${./.}/go.{mod,sum} .";
+
+    ldflags = [ "-s" "-w" ];
+    subPackages = [ "cmd/apimain.go" ];
+    vendorHash = "sha256-NzooRu6AreNzbliofVgjLAnqbsPFVnDXkN70d65jzz8=";
+
+    nativeBuildInputs = with pkgs; [ go-swag ];
+
+    postBuild = ''
+      swag init -g cmd/apimain.go --output docs/api   --instanceName api   --exclude http/controller/admin
+      swag init -g cmd/apimain.go --output docs/admin --instanceName admin --exclude http/controller/api
+
+      ln -s ${frontend} resources/admin
+    '';
+
+    postInstall = "cp -r conf docs resources $out";
+
+    passthru = { inherit frontend; };
+
+    meta = {
+      description = "Custom Rustdesk Api Server";
+      homepage = "https://github.com/lejianwen/rustdesk-api";
+      mainProgram = "apimain";
+    };
+  };
+in
+backend
