f9ec5724c21333c947522f92e0d2cd80294f3be0
Leon Schumacher <leonsch@protonmail.com>
2025-04-28 08:00:34 +0200

    ferroxide: 0.4.0-unstable-2025-02-12 -> 0.5.0

--- packages/ferroxide.nix
+++ packages/ferroxide.nix
@@ -1,15 +1,15 @@
 pkgs: pkgs.buildGoModule rec {
   pname = "ferroxide";
-  version = "0.4.0-unstable-2025-02-12";
+  version = "0.5.0";
 
   src = pkgs.fetchFromGitHub {
     owner = "acheong08";
     repo = pname;
-    rev = "927b502fc3f70f1ea34d6a143e5ebd3e17c0f33a";
-    hash = "sha256-9KsLzcDrBDDNf5DVEiGPd8MZPeTPq41Voj+tWb2CA+M=";
+    rev = "v${version}";
+    hash = "sha256-GShbqcsfM2Wx4Ge4pmdgAUhXIsQSxlG+WE3VKda8ZoU=";
   };
 
-  vendorHash = "sha256-NuN2zgr7Eh8j88lB8i3WJzx6xuCLe9tBZtL5aD6VTcM=";
+  vendorHash = "sha256-YjJdC0ZXNLAUbCoK4L2h0B4EG4y+iYKcTudJkAiOItU=";
 
   subPackages = [ "cmd/${pname}" ];
 
