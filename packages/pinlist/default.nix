2fe30323f9f52907d529a8cc99997435d6fa468d
Leon Schumacher <leonsch@protonmail.com>
2025-08-04 11:38:53 +0200

    pinlist: 0-unstable-2024-08-25 -> 1.1.0

--- packages/pinlist/default.nix
+++ packages/pinlist/default.nix
@@ -1,20 +1,3 @@
-pkgs: pkgs.buildGoModule rec {
-  pname = "pinlist";
-  version = "0-unstable-2024-08-25";
-
-  src = pkgs.fetchFromGitHub {
-    owner = "42LoCo42";
-    repo = pname;
-    rev = "4cb28595176e7c9b4a04192ee57641a41cd03b32";
-    hash = "sha256-LDc3P5wKeHKEgisLO6h7OvXzzjtcscotHNp4TRWBQUs=";
-  };
-
-  ldflags = [ "-s" "-w" ];
-  vendorHash = "sha256-6LEpelMU1eGbjYHQ7LjZqZU/lUGc3tlRen8NgT5vStg=";
-
-  meta = {
-    description = "Super simple text/link pinlist tool";
-    homepage = "https://github.com/42LoCo42/pinlist";
-    mainProgram = pname;
-  };
-}
+pkgs:
+let src = (builtins.getFlake "github:42LoCo42/pinlist/78189b4f2ecc0091a7a133dbb8c3d025e7971cd4"); in
+src.packages.${pkgs.system}.default
