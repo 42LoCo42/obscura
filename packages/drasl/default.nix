ea84077fd09dfd0f011e4c472192da467d3de45a
Leon Schumacher <leonsch@protonmail.com>
2025-08-11 09:10:12 +0200

    drasl: 3.1.1-unstable-2025-07-27 -> 3.2.0

--- packages/drasl/default.nix
+++ packages/drasl/default.nix
@@ -1,9 +1,6 @@
 pkgs:
-let src = (builtins.getFlake "github:unmojang/drasl/c3cb43885f78260c80a1dde211f075a3a2017cd3"); in
+let src = (builtins.getFlake "github:unmojang/drasl/f8eb5da15a1877373a4dd3b68787fd6428003261"); in
 src.packages.${pkgs.system}.drasl.overrideAttrs {
-  version = "3.1.1-unstable-2025-07-27";
-  __intentionallyOverridingVersion = true;
-
   meta = {
     description = "Yggdrasil-compatible API server for Minecraft";
     homepage = "https://github.com/unmojang/drasl";
