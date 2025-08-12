9ab50f362c04aa60537d723ead462da5d1bb1526
Leon Schumacher <leonsch@protonmail.com>
2025-08-12 09:54:23 +0200

    bun2nix: remove IFD

--- packages/bun2nix/default.nix
+++ packages/bun2nix/default.nix
@@ -1,17 +1,27 @@
-pkgs:
-let
+pkgs: pkgs.rustPlatform.buildRustPackage rec {
+  pname = "bun2nix";
+  version = "1.5.1";
+
   src = pkgs.fetchFromGitHub {
     owner = "baileyluTCD";
-    repo = "bun2nix";
-    tag = "1.5.1";
+    repo = pname;
+    tag = version;
     hash = "sha256-rUpcATQ0LiY8IYRndqTlPUhF4YGJH3lM2aMOs5vBDGM=";
   };
-in
-(import "${src}/nix/package.nix" {
-  inherit pkgs;
-  flake = src;
-}).overrideAttrs (old: {
-  meta = old.meta // {
-    mainProgram = old.pname;
+
+  nativeBuildInputs = with pkgs; [
+    pkg-config
+  ];
+
+  buildInputs = with pkgs; [
+    openssl
+  ];
+
+  cargoLock.lockFile = "${src}/Cargo.lock";
+
+  meta = {
+    description = "A fast rust based bun lockfile to nix expression converter.";
+    homepage = "https://github.com/baileyluTCD/bun2nix";
+    mainProgram = pname;
   };
-})
+}
