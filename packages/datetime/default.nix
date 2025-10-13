e9eed33625c21260e2848c435b275582b18559ea
Leon Schumacher <leonsch@protonmail.com>
2025-10-13 18:32:26 +0200

    datetime: 0.2.0 -> 0.2.1

--- packages/datetime/default.nix
+++ packages/datetime/default.nix
@@ -1,5 +1,5 @@
 pkgs: (import (pkgs.fetchzip {
-  url = "https://github.com/42LoCo42/what-datetime-is-it-right-now-dot-com/archive/refs/tags/0.2.0.tar.gz";
-  hash = "sha256-HTKaXNoP8drHOsfJXLRgExNvi1J8D0//LYdqeJvFoF4=";
+  url = "https://github.com/42LoCo42/what-datetime-is-it-right-now-dot-com/archive/refs/tags/0.2.1.tar.gz";
+  hash = "sha256-N2hlTvPOp+GPUeLGn5ZjuWklHuHxrzkPUhZiIwt/aEU=";
 }))
 { inherit pkgs; }
