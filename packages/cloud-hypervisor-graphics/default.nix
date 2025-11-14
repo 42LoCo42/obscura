7de744227d6dee0c977caa74220c28dd90ffa6b2
Leon Schumacher <leonsch@protonmail.com>
2025-11-14 07:41:36 +0100

    unbreak cloud-hypervisor-graphics

--- packages/cloud-hypervisor-graphics/default.nix
+++ packages/cloud-hypervisor-graphics/default.nix
@@ -1,8 +1,8 @@
 pkgs:
 let
   spectrum = fetchTarball {
-    url = "https://spectrum-os.org/git/spectrum/snapshot/spectrum-f14abeb3db2c1c47658fa1809f3fde379e41d632.tar.zst";
-    sha256 = "sha256-hHDDE0z13tIV/aE+XNVnb6tTpt8WQb3OiVZg+/AIt9U=";
+    url = "https://spectrum-os.org/git/spectrum/snapshot/spectrum-4b89596e112d26c4bd60bc46bb45c5d9a0875314.tar.zst";
+    sha256 = "sha256-yguP1BgKqweT0+WAeQ5m+l0SIdlBdKtdJLypASICx90=";
   };
 in
 import "${spectrum}/pkgs/cloud-hypervisor" {
