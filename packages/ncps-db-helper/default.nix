c2088e4ff4673196f8462389cea9b287f1c2baad
Leon Schumacher <leonsch@protonmail.com>
2025-11-16 09:46:19 +0100

    ncps-db-helper: unbreak on aarch64-linux

--- packages/ncps-db-helper/default.nix
+++ packages/ncps-db-helper/default.nix
@@ -1,12 +1,27 @@
-pkgs: pkgs.writeShellApplication rec {
+pkgs:
+let
+  inherit (pkgs.lib)
+    getExe
+    pipe
+    readFile
+    replaceStrings
+    ;
+
+  ncps = pkgs.ncps.overrideAttrs {
+    # TODO https://hydra.nixos.org/job/nixpkgs/trunk/ncps.aarch64-linux
+    doCheck = !pkgs.stdenv.hostPlatform.isAarch64;
+  };
+in
+pkgs.writeShellApplication rec {
   name = "ncps-db-helper";
 
   runtimeInputs = with pkgs; [ coreutils dbmate ];
-  text = pkgs.lib.pipe ./main.sh [
-    builtins.readFile
-    (builtins.replaceStrings [ "@bin@" "@mgr@" ] [
-      (pkgs.lib.getExe pkgs.ncps)
-      "${pkgs.ncps}/share/ncps/db/migrations"
+
+  text = pipe ./main.sh [
+    readFile
+    (replaceStrings [ "@bin@" "@mgr@" ] [
+      (getExe ncps)
+      "${ncps}/share/ncps/db/migrations"
     ])
   ];
 
