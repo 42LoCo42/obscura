a495a4952929b0e9a1eac955b44a0ee69d5dcd30
Leon Schumacher <leonsch@protonmail.com>
2026-01-02 10:53:28 +0100

    cloud-hypervisor-graphics: currently broken

--- packages/cloud-hypervisor-graphics/default.nix
+++ packages/cloud-hypervisor-graphics/default.nix
@@ -4,8 +4,8 @@ let
     url = "https://spectrum-os.org/git/spectrum/snapshot/spectrum-93126ea05e4b45415aee38e2ea4c4264b1c4ccaf.tar.zst";
     sha256 = "sha256-9wzmXlcLz+PE2alNOaAbrIdZTJp8FPoVDKLAFLJ6AO0=";
   };
-in
-import "${spectrum}/pkgs/cloud-hypervisor" {
-  final = pkgs;
-  super = pkgs;
-}
+in pkgs.writeText "cloud-hypervisor" "broken" # TODO spectrum git is down :(
+# import "${spectrum}/pkgs/cloud-hypervisor" {
+#   final = pkgs;
+#   super = pkgs;
+# }
