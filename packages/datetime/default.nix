da76615c840bc29edd0aab8ad82228f7b5ac19f1
Leon Schumacher <leonsch@protonmail.com>
2026-05-15 11:00:23 +0200

    unbreak datetime

--- packages/datetime/default.nix
+++ packages/datetime/default.nix
@@ -1,4 +1,66 @@
-pkgs: (import (fetchTarball {
-  url = "https://github.com/42LoCo42/what-datetime-is-it-right-now-dot-com/archive/refs/tags/0.2.2.tar.gz";
-  sha256 = "sha256-6/9Na4f7h5xPZjWVcVAx6zd810Q1VsK3BDrFY5kjQw4=";
-})) { inherit pkgs; }
+pkgs:
+let
+  inherit (pkgs.lib) getExe;
+  pnpm = pkgs.pnpm_10;
+  inherit (pnpm) nodejs;
+in
+pkgs.stdenv.mkDerivation (drv: {
+  pname = "datetime";
+  version = "0.2.2";
+
+  src = pkgs.fetchFromGitHub {
+    owner = "42LoCo42";
+    repo = "what-datetime-is-it-right-now-dot-com";
+    tag = drv.version;
+    hash = "sha256-6/9Na4f7h5xPZjWVcVAx6zd810Q1VsK3BDrFY5kjQw4=";
+  };
+
+  nativeBuildInputs = with pkgs; [
+    makeBinaryWrapper
+
+    nodejs
+    pnpm
+    pnpmConfigHook
+
+    # for sharp build:
+    pkg-config
+    python3
+  ];
+
+  buildInputs = with pkgs; [
+    vips
+  ];
+
+  pnpmDeps = pkgs.fetchPnpmDeps {
+    inherit pnpm;
+    inherit (drv) pname version src;
+    fetcherVersion = 3;
+    hash = "sha256-kDCSZAoJNenjgCZlcrb8Tx3ZaZRpbYOt2jTGfuy0xkg=";
+  };
+
+  env = {
+    SHARP_FORCE_GLOBAL_LIBVIPS = 1;
+    npm_config_nodedir = nodejs;
+  };
+
+  buildPhase = ''
+    export MAKEFLAGS="-j$NIX_BUILD_CORES"
+    (cd node_modules/.pnpm/node_modules/sharp; npm run install)
+    pnpm build
+  '';
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp -r public $out/app
+
+    makeWrapper                                \
+      ${getExe pkgs.python3} $out/bin/datetime \
+      --add-flags "-m http.server -d $out/app"
+  '';
+
+  meta = {
+    description = "A site that tells you what date and time it is right now";
+    homepage = "https://github.com/42LoCo42/what-datetime-is-it-right-now-dot-com";
+    mainProgram = drv.pname;
+  };
+})
