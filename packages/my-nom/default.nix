d1beca432c446634760ba692e5cd85975f378483
Leon Schumacher <leonsch@protonmail.com>
2026-06-09 08:39:50 +0200

    my-nom: fix uncompleted builds thingy

--- packages/my-nom/default.nix
+++ packages/my-nom/default.nix
@@ -1,12 +1,14 @@
 pkgs: pkgs.infuse pkgs.nix-output-monitor {
   __output = {
-    version.__assign = "2.1.8-unstable-2026-05-22";
+    version.__assign = "2.1.8-unstable-2026-06-08";
 
     src.__assign = pkgs.fetchFromGitHub {
       owner = "maralorn";
       repo = "nix-output-monitor";
-      rev = "0e855e51c1700e35456faa3dee2e50024f602f42";
-      hash = "sha256-8viiPvLkj0vFdG1kgcNuKXoenyTBvKd+GQ62jwbONns=";
+      rev = "388f56120f655a9cf4512e697b2c2afa06fe7434";
+      hash = "sha256-3N+PVFpsnBtQ11Vk9OKm1q9dE0d5fxGsEDyfwoxpYaE=";
     };
+
+    propagatedBuildInputs.__append = [ pkgs.haskellPackages.hinotify ];
   };
 }
