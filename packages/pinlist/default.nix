7301b7c350d0aab752c40265592c2b84aad3db67
Eleonora <leonsch@protonmail.com>
2026-08-17 22:13:12 +0200

    pinlist: remove getFlake usage

--- packages/pinlist/default.nix
+++ packages/pinlist/default.nix
@@ -1,3 +1,23 @@
-pkgs:
-let src = builtins.getFlake "github:42LoCo42/pinlist/35f801f31d0fc3e5844cfcc14465b939e740866b"; in
-src.packages.${pkgs.stdenv.hostPlatform.system}.default
+pkgs: pkgs.buildGoModule (drv: {
+  pname = "pinlist";
+  version = "1.2.0";
+
+  src = pkgs.fetchFromGitHub {
+    owner = "42LoCo42";
+    repo = drv.pname;
+    tag = drv.version;
+    hash = "sha256-vHIIgpk3ngd8ngAi5xDIAlrKSJgfOs69CAEueGftmN4=";
+  };
+
+  __structuredAttrs = true;
+  strictDeps = true;
+
+  ldflags = [ "-s" "-w" ];
+  vendorHash = "sha256-DJxWvd9ABBkxJxqFdr3C2GVUs2Hxmr9GhOBnVj+Ip6Q=";
+
+  meta = {
+    description = "Super simple text/link pinlist tool";
+    homepage = "https://github.com/42LoCo42/pinlist";
+    mainProgram = drv.pname;
+  };
+})
